becbcaaf5cccc957917613c7176f7b4a51d3d2cc
MrHant <mrhantbk@gmail.com>
2015-10-05 19:57:55 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+/**
+ * Created by MrHant on 05.10.2015.
+ */
+var http = require('http');
+var fs = require('fs');
+var chat = require('./chat');
+
+http.createServer(function (req, res) {
+    switch (req.url) {
+        case '/':
+            sendFile("index.html", res);
+            break;
+        case '/subscribe':
+            chat.subscribe(req, res);
+
+            break;
+        case '/publish':
+            var body ='';
+            req
+                .on('readable', function () {
+                    body += req.read();
+                    if (body.length>1e4) {
+                        res.statusCode=413;
+                        res.end("Your message is too big");
+                    }
+                })
+                .on('end', function () {
+                    try{
+                        body=JSON.parse(body);
+                    } catch (e){
+                        res.statusCode=400;
+                        res.end("Bad Request");
+                        return;
+                    }
+
+                    chat.publish(body.message);
+                    res.end("ok");
+                });
+            break;
+        default:
+            res.statusCode = 404;
+            res.end("NotFound");
+    }
+}).listen(1337);
+
+function sendFile(fileName, res) {
+    var fileStream = fs.createReadStream(fileName);
+    fileStream
+        .on('error', function () {
+            res.statusCode = 500;
+            res.end("Server error");
+        })
+        .pipe(res);
+    res.on('close', function () {
+        file.destroy();
+    })
+}
